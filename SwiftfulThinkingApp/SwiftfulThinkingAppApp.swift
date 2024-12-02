256e5350dc927fec31f1fd97864b05d154d71353
Abner <baluyutabner@gmail.com>
2024-12-02 11:18:37 +0800

    Initial Commit

---
+++ SwiftfulThinkingApp/SwiftfulThinkingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAppApp.swift
+//  SwiftfulThinkingApp
+//
+//  Created by Abner Lawrence Baluyut on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
